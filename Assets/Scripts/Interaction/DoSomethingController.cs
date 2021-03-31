3178c4a545ad4d0de22be8a4be49c030f669ede8
Emma <emma.seeger@me.com>
2021-03-31 12:07:44 +0200

    Added a planting action to interactable plant
    had to create a new prefab for interactable bed. How can we have nested prefabs? I put one interactable bed with planting action into the scene. Removed watertank.

--- Assets/Scripts/Interaction/DoSomethingController.cs
+++ Assets/Scripts/Interaction/DoSomethingController.cs
@@ -6,8 +6,13 @@ using UnityEngine.iOS;
 public class DoSomethingController : MonoBehaviour
 {
 
-    [SerializeField] private ParticleSystem InteractionCue = null;
+    [SerializeField] 
+    private ParticleSystem InteractionCue = null;
     
+    [SerializeField]
+    private GameObject _plantPrefab;
+    
+    //action for testing
     public void Action()
     {
         Debug.Log("ACTION");
@@ -16,4 +21,19 @@ public class DoSomethingController : MonoBehaviour
         // Harvest
         // Water...
     }
+    
+    //use "p" for planting
+    public void Plant()
+    {
+        Debug.Log("Planting");
+        //bed spawns a new plant on top
+        Instantiate(_plantPrefab, transform.position + new Vector3(0,-0.3f,0),  Quaternion.identity, this.transform);
+
+        InteractionCue.Play();
+        
+    }
+    
+    //watering
+    
+    //planting
 }
