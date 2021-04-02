e064004af7168b103fdb871b324e97c9922693fc
Matthias <matzerichter@t-online.de>
2021-04-02 15:56:38 +0200

    Add finite state machine for newinteractionbed

--- Assets/Scripts/Interactables/Bed.cs
+++ Assets/Scripts/Interactables/Bed.cs
@@ -12,26 +12,80 @@ public class Bed : InteractableItemBaseClass {
     [SerializeField] public float interactionDelay = 20;
     private bool isPlanted = false;
     private float lastInteractionTime = 0f;
+    
+    //finite state machine for bed
+    private enum BedFSM
+    {
+        plain,
+        planted,
+        growth
+            
+    }
+    // default state
+    BedFSM bedMode = BedFSM.plain;
 
     public override void OnInteract()
     {
+        
+        //perform an action depending on the mode
+        if (bedMode == BedFSM.plain)
+        {
+            Planting();
+            bedMode = BedFSM.planted;
+    
+        }
+            
+        else if (bedMode == BedFSM.planted)
+        {
+            Watering();
+            bedMode = BedFSM.growth;
+    
+        }
+            
+        else if (bedMode == BedFSM.growth)
+        {
+            Harvesting();
+            bedMode = BedFSM.plain;
+        }
 
-            
-            if (!isPlanted) Plant();
-            
-            _interactionCue.Play();
+        
 
             // GetComponent<Animator>().SetBool("open", mIsOpen);
         
     }
     
-    private void Plant()
-    {
-        //bed spawns a new plant on top
-        Instantiate(_plantPrefab, transform.position + new Vector3(0,0.1f,0),  Quaternion.identity, this.transform);
-        Debug.Log("Planted");
 
-        isPlanted = !isPlanted;
-        InteractText += isPlanted ? "to plant" : "to harvest";
-    }
+    
+
+    
+
+
+        public void Planting()
+        {
+            Debug.Log("Planting");
+            // GameObject plant = (GameObject)Instantiate(Resources.Load("Plant")); 
+            Instantiate(_plantPrefab, transform.position + new Vector3(0, 0.1f, 0), Quaternion.identity, this.transform);
+            
+            _interactionCue.Play();
+            
+        }
+    
+        public void Watering()
+        {
+            Debug.Log("Watering");
+            
+            _interactionCue.Play();
+        }
+        
+        public void Harvesting()
+        {
+            Debug.Log("Harvesting");
+            
+            _interactionCue.Play();
+        }
+        
+
 }
+
+
+
