9253290620fcd7ea03a50991704e4159e189c4f1
Emma <emma.seeger@me.com>
2021-04-07 13:08:32 +0200

    Added plant health, needed care and plant death

--- Assets/Scripts/Interactables/Plant.cs
+++ Assets/Scripts/Interactables/Plant.cs
@@ -2,34 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Plant : MonoBehaviour {
+public class Plant : MonoBehaviour
+{
 
-
-    
-
-    [SerializeField] public float maxSize = 6f;
-    [SerializeField] public float BaseGrowthRate = 0.1f;
-    
-    
-    private Vector3 _growingDirection = new Vector3(0,1,0);
-    private float currentGrowthRate;
-    private float growthTickRate = 2f;
-    
+    //basic parameters
+    private float humidity = 0f;
     private bool watered = false;
-
     public int obstructiveWeeds = 0;
-    
     public bool growingCondition = true; //Perhaps unnecessary
     public bool readyToHarvest = false;
-    
+    public float care = 0f;
+
+
+    //plant specific parameters
+    [SerializeField] public float maxSize = 6f;
+    [SerializeField] public float BaseGrowthRate = 0.1f;
+
+    private Vector3 _growingDirection = new Vector3(0, 1, 0);
+    private float currentGrowthRate;
+    private float growthTickRate = 2f;
+    private float needed_care = 3f; //e.g how often we need to water, to harvest
+    private float health = 3f; //if health == 0 plant dies
+
+
     void Start()
     {
         currentGrowthRate = BaseGrowthRate;
         InvokeRepeating("Grow", growthTickRate, growthTickRate);
         // There is also "Cancel Invoke" might be helpful. But changing the growing condition is already enough I think. Could also be replaced by a coroutine
     }
-    
-    void Grow() {
+
+    void Grow()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
         //Only grow until max size
         if (this.transform.localScale.y >= maxSize)
         {
@@ -37,19 +46,44 @@ public class Plant : MonoBehaviour {
             readyToHarvest = true;
         }
 
-        if (obstructiveWeeds > 3) return;
-        
+        //Weeds influence plant growth
+        if (obstructiveWeeds == 5)
+        {
+            currentGrowthRate = 0;
+            health -= 1;
+        }
+
+        if (obstructiveWeeds >= 10)
+        {
+            health = 0;
+        }
+
         if (!growingCondition) return;
-        
+        //if (this.transform.localScale.y <= ((maxSize / needed_care) * care)) // n deckel aufs wachsen draufpacken, wenn nicht genug care raufpacken
+
         this.transform.localScale += (currentGrowthRate * _growingDirection);
     }
 
+
+
+
+
     public void Water()
     {
-        if (watered) return;
+        if (watered)
+        {
+            //overwatering
+            health -= 1;
+        }
+        else
+        {
+            watered = true;
+            currentGrowthRate *= 4;
+            care += 1;
 
-        watered = true;
-        currentGrowthRate *= 4;
+        }
+
+        
     }
     
     public void Dry()
@@ -57,6 +91,7 @@ public class Plant : MonoBehaviour {
         watered = false;
         currentGrowthRate = BaseGrowthRate;
     }
+
 
     
 }
