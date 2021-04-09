f17e3a43f8a9d6300429902a8f6200d1be94f560
Lukas Re <lukasrehberg@live.de>
2021-04-09 14:46:41 +0200

    Einzelnes Beet entfernt

--- Assets/Scripts/PadToDo.cs
+++ Assets/Scripts/PadToDo.cs
@@ -1,25 +1,12 @@
 /*
  Dringlichkeitsstufe 1 sehr hoch - 5 niedrig
 
-game logic refinen                                          1       done
-ernten debuggen  only if ready to harvest                   1       done
-i für instructions                                          1       done
-randbedingungen & z !< 0                                    1       done
-animation player wieder hinzufügen (ohne jump)              1       done
+
 pflanzen verschöenern                                       1
-verschiedene pflanzen in logic                              2       done
-        Use SeedListener to add vegetables, use Plant.Transform() to add growing states 
-        (switch (vegetable)
-            if size == 1 : change zylinder to vegetable)
-                            +transform
-            if size == 2: transform
-            if size == 3: transform
 In Inventory über Seedlistener Saattütenimages ändern       2
-remove vacant spaces in inventory                           2       done
 CameraMovement Debuggen (auch weniger sensitiv?)            3
 erntezähler                                                 3
 challenges einrichten                                       3
-einzelnes Beet entfernen                                    3
 wenn schaufel -> neues beet anlegen                         4
        Idee dazu: inaktive grüne Beetreihen die sich auch so heben, als könnte man mit denen
        interagieren. Wenn Dig() dann werden die aktiv. Wenn man überall Beete erstellen kann
@@ -33,6 +20,21 @@ give plants a collider                                     ? wollen wir das übe
 
 
 
+Already done (and explanations):
 
+game logic refinen                                          1       done
+ernten debuggen  only if ready to harvest                   1       done
+i für instructions                                          1       done
+randbedingungen & z !< 0                                    1       done
+animation player wieder hinzufügen (ohne jump)              1       done
+verschiedene pflanzen in logic                              2       done
+        Use SeedListener to add vegetables, use Plant.Transform() to add growing states 
+        (switch (vegetable)
+            if size == 1 : change zylinder to vegetable)
+                            +transform
+            if size == 2: transform
+            if size == 3: transform
+remove vacant spaces in inventory                           2       done
+einzelnes Beet entfernen                                    3       done
 
 */
