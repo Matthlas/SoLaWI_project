b14fb63d25420e8a3cc1863a977c9c0ff834139e
Lukas Re <lukasrehberg@live.de>
2021-04-11 16:37:36 +0200

    only for me gitattributes, my git is sooo slow

---
+++ Assets/Scripts/ChallengeManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChallengeManager : MonoBehaviour
+{
+    private int challengeNr = 1;
+    void Start()
+    {
+        newChallenge();
+    }
+
+    public void newChallenge()
+    {
+        
+    }
+}
