047099d54dc752cf8cbf6c91fae51666d14884b0
Matthias <matzerichter@t-online.de>
2021-04-12 13:48:23 +0200

    Guitar can float now

--- Assets/Scripts/GuitarHandler.cs
+++ Assets/Scripts/GuitarHandler.cs
@@ -12,8 +12,26 @@ public class GuitarHandler : InteractableItemBaseClass
     private bool whichAudio = true;
 
     private bool isPlaying = false;
+    
+    float originalY;
 
+    public float floatStrength = 1; 
 
+    void Start()
+    {
+        this.originalY = this.transform.position.y;
+    }
+
+    void Update()
+    {
+        if (!selected)
+        {
+            transform.position = new Vector3(transform.position.x,
+                originalY + ((float) Math.Sin(Time.time) * floatStrength),
+                transform.position.z);
+        }
+    }
+    
     public override void OnInteract()
     {
         if (!isPlaying)
--- Assets/Scripts/InteractabeItemBaseClass.cs
+++ Assets/Scripts/InteractabeItemBaseClass.cs
@@ -24,6 +24,8 @@ public class InteractableItemBaseClass : MonoBehaviour
 
     public ParticleSystem _interactionCue;
 
+    internal bool selected = false;
+
     public virtual void OnInteractAnimation(Animator animator)
     {
         
@@ -39,12 +41,14 @@ public class InteractableItemBaseClass : MonoBehaviour
     {
         //this.GetComponent<MeshRenderer>().material.SetFloat("_Metallic", 0.5f);
         this.transform.position += new Vector3(0,0.1f,0);
+        selected = true;
     }
 
     public virtual void Unselect()
     {
         //this.GetComponent<MeshRenderer>().material.SetFloat("_Metallic", 0f);
         this.transform.position -= new Vector3(0,0.1f,0);
+        selected = false;
     }
 
     public virtual bool CanInteract(Collider other)
