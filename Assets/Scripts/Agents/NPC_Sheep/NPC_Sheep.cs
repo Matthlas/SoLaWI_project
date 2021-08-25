b76d5d3ec90057ec48f8df7fda03f1f8969c78a9
Matthias <matzerichter@t-online.de>
2021-08-25 14:46:44 +0200

    Implementation of an adaptable NPC structure applied for sheep NPCS

---
+++ Assets/Scripts/Agents/NPC_Sheep/NPC_Sheep.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+//NPC structure inspired and adapted from
+//https://github.com/onewheelstudio/Programming-Patterns/tree/master/Programming%20Patterns/Assets/State%20Pattern/Scripts/FSM
+//https://www.youtube.com/watch?v=nnrOhb5UdRc
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NPC_Sheep : MonoBehaviour
+{
+    [SerializeField]
+    private string currentStateName; //Displays current state in Game Manager
+    private INPCState currentState;
+
+    // States --> initialize all states this NPC needs
+    public WanderState wanderState = new WanderState();
+    public FollowState followState = new FollowState();
+    public AvoidState avoidState = new AvoidState();
+
+    public NavMeshAgent navAgent;
+
+    public Vector3 nextLocation;
+    
+    //Avoiding
+    public GameObject Avoiding;
+    public float AvoidingDistance = 3.0f;
+
+    // Following
+    public GameObject Following;
+    public float FollowDistance = 5.0f;
+    
+    //Wandering
+    public float ExplorationRadius = 2.0f;
+    public float chillTime = 5.0f;
+    public bool chillin = false;
+    
+
+    //Animation
+    private Animator mAnimator;
+    
+    
+    private void OnEnable()
+    {
+        ChillABit();
+        currentState = wanderState;
+        if (mAnimator == null)
+            mAnimator = GetComponent<Animator>();
+        if (navAgent == null)
+            navAgent = GetComponent<NavMeshAgent>();
+    }
+    
+    private bool IsNavMeshMoving
+    { 
+        get
+        { 
+            return navAgent.velocity.magnitude > 0.1f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Each frame the current state is executed and if a state transition is triggered
+        //in the execution of the state the state is changed
+        currentState = currentState.DoState(this);
+        currentStateName = currentState.ToString();
+        
+        //Walk animation if the agent is moving
+        mAnimator.SetBool("walk", IsNavMeshMoving);
+    }
+    
+    public bool FarFromFollowing()
+    {
+        return Vector3.Distance(transform.position, Following.transform.position) > FollowDistance;
+    }
+    
+    public bool CloseToAvoiding()
+    {
+        return Vector3.Distance(transform.position, Avoiding.transform.position) < AvoidingDistance;
+    }
+
+    //Method used to delay the wandering by *ChillTime*. Has to be called in the NPC class
+    //since Invoke is a method from "MonoBehaviour"
+    public void ChillABit()
+    {
+        if (!chillin)
+        {
+            chillin = true;
+            Invoke("StopChilling", chillTime + Random.Range(-chillTime/2, chillTime));
+        }
+    }
+    
+    public void StopChilling()
+    {
+        chillin = false;
+        navAgent.isStopped = false;
+    }
+}
---
+++ Assets/Scripts/Agents/NPC_Sheep/WanderState.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+using System.Collections.Generic;
+
+public class WanderState : INPCState
+{
+    public INPCState DoState(NPC_Sheep npc)
+    {
+        //Guarantee NavMeshAgent
+        if (npc.navAgent == null)
+        {
+            npc.nextLocation = npc.transform.position;
+            npc.navAgent = npc.GetComponent<NavMeshAgent>();
+        }
+
+        //Execute wandering specific actions
+        DoWander(npc);
+        
+        //Conditions for state transitions
+        if (npc.CloseToAvoiding())
+            return npc.avoidState;
+        else if (npc.FarFromFollowing())
+            return npc.followState;
+        else
+            return npc.wanderState;
+    }
+
+    public void SetNewExplorationPoint(NPC_Sheep npc)
+    {
+        Vector3 explorePosition = new Vector3(Random.Range(-npc.ExplorationRadius, npc.ExplorationRadius), Random.Range(-npc.ExplorationRadius, npc.ExplorationRadius), 1);
+        npc.nextLocation = npc.transform.position + explorePosition;
+
+        if (NavMesh.SamplePosition(npc.nextLocation, out NavMeshHit hit, 5f, NavMesh.AllAreas))
+        {
+            npc.nextLocation = hit.position;
+            npc.navAgent.SetDestination(npc.nextLocation);
+        }
+    }
+
+    private void DoWander(NPC_Sheep npc)
+    {
+        //if close to current target chill and then choose next location
+        if (npc.navAgent.remainingDistance < npc.FollowDistance/2)
+        {
+            if (!npc.chillin)
+            {
+                SetNewExplorationPoint(npc);
+                //Randomized chill time for more dynamic behaviour
+                npc.ChillABit();
+            }
+            
+        }
+    }
+}
