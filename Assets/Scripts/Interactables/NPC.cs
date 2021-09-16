9279f7a1d18100f92193c1cb1481def5fcc38730
Emma <emma.seeger@me.com>
2021-09-16 14:03:09 +0200

    Add Quest Object

--- Assets/Scripts/Interactables/NPC.cs
+++ Assets/Scripts/Interactables/NPC.cs
@@ -8,19 +8,30 @@ using Cinemachine;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
+
+
 public class NPC : InteractableItemBaseClass
 {
-
+    //maybe make array of dialogues?
     public Dialogue dialogue1;
     public Dialogue dialogue2;
+    public Dialogue dialogue3;
+
+    [SerializeField]
+    public GameObject QuestItem;
+    
+    public bool Qcompleted = false;
     //finite state machine for Quest/NPC status
     public enum NPCFSM {
         inactive,
         pending,
-        completed
+        completed,
     }
     // default state
     public NPCFSM NPCMode = NPCFSM.inactive;
+ 
+    
+    
     
     
 
@@ -30,18 +41,32 @@ public class NPC : InteractableItemBaseClass
     }
 
     public override void OnInteract()
-    {   
-        if (NPCMode == NPCFSM.pending)
-        {
-            triggerDialogue(dialogue2);
-            //insert something like: if pressed continue then change npc mode
-            NPCMode = NPCFSM.pending;
-        }
+    {
         if (NPCMode == NPCFSM.inactive)
         {
             triggerDialogue(dialogue1);
             //insert something like: if pressed continue then change npc mode
+            
             NPCMode = NPCFSM.pending;
+        }
+        if (NPCMode == NPCFSM.pending)
+        {
+            triggerDialogue(dialogue2);
+            //insert something like: if pressed continue then change npc mode
+            //spawns quest Item
+            MeshRenderer spawnItem = QuestItem.GetComponent<MeshRenderer>();
+            spawnItem.enabled = true;
+            
+            if (Qcompleted == true)
+            {
+                NPCMode = NPCFSM.completed;
+            }
+            
+        }
+        if (NPCMode == NPCFSM.completed)
+        {
+            triggerDialogue(dialogue3);
+            //insert something like: if pressed continue then change npc mod
         }
         else
         {
