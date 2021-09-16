ee33f4f9af2c5be8320bb8df3db83b5045b93ba0
Emma <emma.seeger@me.com>
2021-09-16 14:50:30 +0200

    Added first Quest
    Still very buggy

---
+++ Assets/Scripts/Interactables/QuestItem.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using Cinemachine;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+
+public class QuestItem : InteractableItemBaseClass
+{
+
+    public override void OnInteract()
+    {
+        //destroy game object
+        //Destroy(gameObject);
+        MeshRenderer spawnItem = gameObject.GetComponent<MeshRenderer>();
+        spawnItem.enabled = false;
+        //play cue
+        //set quest to completed /alternative: directly change mode of npc
+        NPC.Qcompleted = true;
+
+
+    }
+
+
+
+
+
+
+
+}
