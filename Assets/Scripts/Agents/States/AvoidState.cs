2ebad09b7a0510ecf3963e686d8a369b43d1a998
Matthias <matzerichter@t-online.de>
2021-09-25 17:53:58 +0200

    Tweak avoiding state slightly to accept multiple targets. Animation impo
    Also animation imports

--- Assets/Scripts/Agents/States/AvoidState.cs
+++ Assets/Scripts/Agents/States/AvoidState.cs
@@ -2,43 +2,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(NPC_BaseClass))]
 [RequireComponent(typeof(IdleBasicState))]
+
 public class AvoidState : NPCState
 {
     [HideInInspector] public NPC_BaseClass npc;
     [HideInInspector] private IdleBasicState idleState;
-    [HideInInspector] private AvoidState avoidState;
-    
+
     //Avoiding variables
-    public GameObject Avoiding;
-    // public List<GameObject> objectsToAvoid;
+    public List<GameObject> objectsToAvoid;
     public float AvoidingDistance = 3.0f;
-    private float[] distances;
+    private GameObject Avoiding;
     
     private void OnEnable()
     {
+        // Instantiate all variables
         npc = this.GetComponent<NPC_BaseClass>();
         idleState = this.GetComponent<IdleBasicState>();
-        avoidState = this;
-        // distances = new float[objectsToAvoid.Count];
+        Avoiding = null;
     }
     
 
     public override void ExecuteStateAction()
     {
-        //Calculate direction vector to avoiding and set new destination
-        Vector3 dirToAvoiding = npc.transform.position - avoidState.Avoiding.transform.position;
-        Vector3 newPos = npc.transform.position + dirToAvoiding;
+        if (Avoiding != null)
+        {
+            //Calculate direction vector to avoiding and set new destination
+            Vector3 dirToAvoiding = npc.transform.position - Avoiding.transform.position;
+            Vector3 newPos = npc.transform.position + dirToAvoiding;
         
-        if (npc.navAgent.destination != newPos)
-            npc.navAgent.SetDestination(newPos);
+            if (npc.navAgent.destination != newPos)
+                npc.navAgent.SetDestination(newPos);
+        }
+
 
     }
 
     public override NPCState TransitionToState()
     {
+        //Define transition conditions
         if (CloseToAvoiding())
-            return avoidState;
+            return this;
         else
             return idleState;
 
@@ -46,13 +51,23 @@ public class AvoidState : NPCState
     
     public bool CloseToAvoiding()
     {
-        // float[] distances = new float[objectsToAvoid.Count];
-        // foreach (var object in objectsToAvoid)
-        // {
-        //     
-        // }
+        //Calculate distances to all objects to avoid
+        //Make the closest object the one to Avoid
+        float closest_dist = AvoidingDistance + 1;
+        Avoiding = null;
+        foreach (var obj in objectsToAvoid)
+        {
+            float distToObj = Vector3.Distance(transform.position, obj.transform.position);
+            if (distToObj < closest_dist)
+            {
+                closest_dist = distToObj;
+                Avoiding = obj;
+            }
+                
+        }
+        //Return true if there is an object to avoid, false otherwise
         if (Avoiding == null)
             return false;
-        return Vector3.Distance(transform.position, Avoiding.transform.position) < AvoidingDistance;
+        return closest_dist < AvoidingDistance;
     }
 }
