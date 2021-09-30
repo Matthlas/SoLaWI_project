3716539a87ec1d47c7bf8b06e33408ea08830c85
Matthias <matzerichter@t-online.de>
2021-09-30 13:29:25 +0200

    Minor bug fixes for building the game

--- Assets/Scripts/Agents/MeetPlayerState.cs
+++ Assets/Scripts/Agents/MeetPlayerState.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design.Serialization;
-using UnityEditor.Timeline.Actions;
 using UnityEngine;
 using UnityEngine.AI;
 
--- Assets/Scripts/Agents/NPC_Citizen1/NPC_Citizen1.cs
+++ Assets/Scripts/Agents/NPC_Citizen1/NPC_Citizen1.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Timeline.Actions;
 using UnityEngine;
 using UnityEngine.AI;
 using Random = System.Random;
